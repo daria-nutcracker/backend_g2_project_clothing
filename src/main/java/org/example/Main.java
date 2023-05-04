85eddbcc0c42d89641c6d06ba32291202b8ca602
Daria <dsshchelokova.42@gmail.com>
2023-05-04 22:13:22 +0300

    Описала сущность Товар

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.models.Goods;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет! Я - интернет-магазин.");
+        //Goods goods = new Goods(1, "Платье женское", 13.4, 5);
+        //System.out.println(goods.toString());
     }
 }
--- src/main/java/org/example/models/Goods.java
+++ src/main/java/org/example/models/Goods.java
@@ -1,7 +1,58 @@
 package org.example.models;
 
+import java.text.DecimalFormat;
+
 /**
  Класс - реализация сущности товаров
  */
 public class Goods {
+    private int id;
+    private String name;
+    private double price;
+    private int count;
+
+    public Goods(int id, String name, double price, int count) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        String formattedPrice = new DecimalFormat("#0.00").format(price);
+        return "Товар: " + name + " id = " + id + " стоимостью " + formattedPrice + " в количестве " + count;
+    }
 }
