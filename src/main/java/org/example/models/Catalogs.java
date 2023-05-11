a160af7a2d408a3011b9b22371782e0ada76ad77
Alya Zagatskaya <alya.zagacki@gmail.com>
2023-05-11 11:57:06 +0300

    add addGoods method to Catalogs

--- src/main/java/org/example/models/Catalogs.java
+++ src/main/java/org/example/models/Catalogs.java
@@ -10,10 +10,20 @@ public class Catalogs {
     String name;
     ArrayList<Goods> goods;
 
-    public Catalogs(int id, String name, ArrayList<Goods> goods) {
+    public Catalogs(int id, String name) {
         this.id = id;
         this.name = name;
-        this.goods = goods;
+        this.goods = new ArrayList<Goods>();
+    }
+
+    public void addGoods(Goods goods){
+        this.goods.add(goods);
+    }
+    @Override
+    public String toString() {
+        return "Каталог " + name + " {\n"
+                 + "id: " + id + "\n"
+                + "goods: " + goods.toString() + " }\n";
     }
 
     public Catalogs() {
