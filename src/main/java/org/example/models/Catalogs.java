4b1593ed337f34a2abc3325d7320461b23963c2c
Alya Zagatskaya <alya.zagacki@gmail.com>
2023-05-11 10:04:32 +0300

    add getters & setters to Catalogs

--- src/main/java/org/example/models/Catalogs.java
+++ src/main/java/org/example/models/Catalogs.java
@@ -1,7 +1,42 @@
 package org.example.models;
 
+import java.util.ArrayList;
+
 /**
  Класс - реализация сущности каталогов
  */
 public class Catalogs {
+    int id;
+    String name;
+    ArrayList<Goods> goods;
+
+    public Catalogs(int id, String name, ArrayList<Goods> goods) {
+        this.id = id;
+        this.name = name;
+        this.goods = goods;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Goods> getGoods() {
+        return goods;
+    }
+
+    public void setGoods(ArrayList<Goods> goods) {
+        this.goods = goods;
+    }
 }
