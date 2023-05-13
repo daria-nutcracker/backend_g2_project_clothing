fd8097f3e6c4134d416a46cc091b0689691c3a03
Iuliia Ia <me@iablochkina.ru>
2023-05-13 10:45:06 +0300

    Внесла информацию о пользователях

--- src/main/java/org/example/models/Users.java
+++ src/main/java/org/example/models/Users.java
@@ -4,4 +4,18 @@ package org.example.models;
  Класс - реализация сущности пользователей
  */
 public class Users {
+    public static void main(String[] args) {
+        // создаем пользователей
+        User user1 = new User("Митя");
+        User user2 = new User("Катя");
+        User user3 = new User("Дима");
+        User user4 = new User("Лилия");
+        User user5 = new User("Миша");
+    }
+}
+class User {
+    private String name;
+    public User(String name) {
+        this.name = name;
+    }
 }
